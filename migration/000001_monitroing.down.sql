5995d9eb5970de741d46a77f8e645deef172ebaa
Smirnov Nikita <smirnovn@mtsbank.ru>
2024-04-18 00:01:02 +0300

    fix

--- migration/000001_monitroing.down.sql
+++ migration/000001_monitroing.down.sql
@@ -2,6 +2,6 @@
 drop type if exists role_user;
 drop type  if exists role;
 
-drop table message;
-drop table channel;
-drop table "user";
+drop table if exists message;
+drop table if exists channel;
+drop table if exists "user";
